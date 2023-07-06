cce6f3f8c361fc093f25d1e88885a50fe8849713
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 <saggamsuresh3@gmail.com>
2023-07-06 14:28:58 +0000

    First Names of People

---
+++ names/index.js
@@ -0,0 +1,6 @@
+const a = require("../country/state/city/index");
+const b = require("../utilities/utils/index");
+const getPeopleInCity = (c) => {
+  return b(c);
+};
+module.exports = getPeopleInCity;
